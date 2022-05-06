3e3c22d15a1c3699e1fc1e65324ba447194e7ec2
ajl0023 <austinlee199723@yahoo.com>
2022-05-05 19:21:38 -0700

    added responsiveness changes/functionality when user leaves

--- commands.bat
+++ commands.bat
@@ -1,1 +1,1 @@
-cd client && npm run build && cd .. && git add . && git commit -m "added responsiveness changes/functionality when user leaves" && git push origin master
+cd client && npm run build && cd .. && git add . && git commit -m "added responsiveness changes/functionality when user leaves" && git push origin main && git push heroku main
